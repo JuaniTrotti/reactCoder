e777b8db195b0163cb73f3e3b92cd4b25c15bad1
juani <juani.trotti@gmail.com>
2022-02-28 19:22:59 -0300

    desafio2

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <header className="header cFlex">
+            <section className="containerNav cFlex">
+                <h2>Aripto</h2>
+                <nav>
+                    <a href="#" className="link">Shop</a>
+                    <a href="#" className="link">About Us</a>
+                </nav>
+            </section>
+        </header>
+    )
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
+//1) tener la variable react en el scope\
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//2) tener la variable reactDOM en el scope
+import ReactDOM from 'react-dom'
+import App from './App'
+import "./components/style.scss"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//4) renderizar App en el DOM
+ReactDOM.render(<App/> ,document.querySelector('#root')) 
