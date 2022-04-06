a333281e718d93675552d815e189ad558a25ce19
juani <juani.trotti@gmail.com>
2022-04-06 01:22:16 -0300

    maquetado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import Main from "./components/Main"
+import FooterContainer from "./components/FooterContainer"
 import { BrowserRouter } from "react-router-dom"
 import Context from "./components/Context"
 
@@ -7,8 +8,13 @@ const App = () => {
     return (
         <Context>
             <BrowserRouter>
-                <NavBar/>
-                <Main/>
+                <div className="responsiveController cFlex">
+                    <div className="paginaContainer cFlex">
+                        <NavBar/>
+                        <Main/>
+                        <FooterContainer/>
+                    </div>
+                </div>
             </BrowserRouter>
         </Context>
     )
---
+++ src/components/FooterContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FooterContainer = () => {
+  return (
+    <div className='footerContainer cFlex'> 
+    </div>
+  )
+}
+
+export default FooterContainer
